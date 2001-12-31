b3197a59e483068f9fb5a7856faf91d493202406
reenoo <reenoo>
2001-12-31 21:45:23 +0000

    minor fixes

--- server/drivers/curses_drv.c
+++ server/drivers/curses_drv.c
@@ -24,8 +24,6 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 */
 
-/*Configfile support added by Rene Wagner (C) 2001*/
-
 /*
 Different implementations of (n)curses available on:
 OpenBSD: 
