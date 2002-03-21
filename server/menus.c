575b5a003ab442267076625a844f6c1900934434
reenoo <reenoo>
2002-03-21 15:55:11 +0000

    forgot adding my copyrights

--- server/menus.c
+++ server/menus.c
@@ -6,6 +6,7 @@
  * COPYING file distributed with this package.
  *
  * Copyright (c) 1999, William Ferrell, Scott Scriven
+ *               2002, Rene Wagner
  *
  *
  * Defines the default menus the server provides.  Also includes the
