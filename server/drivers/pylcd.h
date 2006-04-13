82b8572f85b94911b163c2c1f351f53ff5845893
marschap <marschap>
2006-04-13 07:22:56 +0000

    sync with main

---
+++ server/drivers/pylcd.h
@@ -0,0 +1,60 @@
+/* pylcd.h */
+
+/*
+ This is the header file of the LCDproc driver for
+ the "pyramid" LCD device from Pyramid.
+
+ Copyright (C) 2005 Silvan Marco Fin <silvan@kernelconcepts.de>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+
+ */
+
+#ifndef PYLCD_H
+#define PYLCD_H
+
+#include "lcd.h"
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#define MAXCOUNT 10
+#define WIDTH 16
+#define HEIGHT 2
+
+
+typedef struct
+{
+    int FD;
+    char device[255];
+    int width;
+    int height;
+    fd_set rdfs;
+    struct timeval timeout;
+    char framebuffer[WIDTH*HEIGHT+2];
+    int FB_modified;
+    int LEDtoggle;
+    char last_key_pressed[6];
+    unsigned long long last_key_time;
+    unsigned long long last_buf_time;
+    int C_x;
+    int C_y;
+    int C_state;
+    char led[7];
+} PrivateData;
+
+#endif
+
--- shared/sockets.c
+++ shared/sockets.c
@@ -1,18 +1,24 @@
+#include "config.h"
 #include <unistd.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#ifndef WINSOCK2
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <sys/types.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#else
+#include <winsock2.h>
+#endif
+#include <stdarg.h>
 #include <fcntl.h>
 
-#include "debug.h"
+#include "report.h"
 #include "sockets.h"
 
 /**************************************************
@@ -27,16 +33,16 @@
 typedef struct sockaddr_in sockaddr_in;
 
 static int
-sock_init_sockaddr (sockaddr_in * name, const char *hostname, unsigned short int port)
+sock_init_sockaddr (sockaddr_in *name, const char *hostname, unsigned short int port)
 {
 	struct hostent *hostinfo;
 
-	memset (name, 0, sizeof (*name));
+	memset (name, '\0', sizeof (*name));
 	name->sin_family = AF_INET;
 	name->sin_port = htons (port);
 	hostinfo = gethostbyname (hostname);
 	if (hostinfo == NULL) {
-		fprintf (stderr, "sock_init_sockaddr: Unknown host %s.\n", hostname);
+		report (RPT_ERR, "sock_init_sockaddr: Unknown host %s.", hostname);
 		return -1;
 	}
 	name->sin_addr = *(struct in_addr *) hostinfo->h_addr;
@@ -53,24 +59,42 @@ sock_connect (char *host, unsigned short int port)
 	int sock;
 	int err = 0;
 
-	debug ("sock_connect: Creating socket\n");
+	report (RPT_DEBUG, "sock_connect: Creating socket");
 	sock = socket (PF_INET, SOCK_STREAM, 0);
+#ifdef WINSOCK2        
+        if (sock == INVALID_SOCKET) {
+#else
 	if (sock < 0) {
-		perror ("sock_connect: Error creating socket");
+#endif
+		report (RPT_ERR, "sock_connect: Error creating socket");
 		return sock;
 	}
-	debug ("sock_connect: Created socket (%i)\n", sock);
+	debug (RPT_DEBUG, "sock_connect: Created socket (%i)", sock);
 
-	sock_init_sockaddr (&servername, host, port);
+	if (sock_init_sockaddr (&servername, host, port) < 0)
+		return -1;
 
 	err = connect (sock, (struct sockaddr *) &servername, sizeof (servername));
+#ifdef WINSOCK2        
+	if (err == INVALID_SOCKET) {
+#else
 	if (err < 0) {
-		perror ("sock_connect: connect failed");
-		shutdown (sock, 2);
+#endif
+		report (RPT_ERR, "sock_connect: connect failed");
+		shutdown (sock, SHUT_RDWR);
 		return 0;					  // Normal exit if server doesn't exist...
 	}
 
+#ifndef WINSOCK2        
 	fcntl (sock, F_SETFL, O_NONBLOCK);
+#else
+        {
+                unsigned long tmp = 1;
+		
+                if (ioctlsocket(sock, FIONBIO, &tmp) == SOCKET_ERROR)
+                        report(RPT_ERR, "sock_connect: Error setting socket to non-blocking");
+        }
+#endif
 
 	return sock;
 }
@@ -80,46 +104,41 @@ sock_close (int fd)
 {
 	int err;
 
-	err = shutdown (fd, 2);
+	err = shutdown (fd, SHUT_RDWR);
 	if (!err)
 		close (fd);
 
 	return err;
 }
 
+
+/**  send printf-like formatted output */
+int
+sock_printf(int fd, const char *format, .../*args*/ )
+{
+	char buf[MAXMSG];
+	va_list ap;
+	int size = 0;
+
+	va_start(ap, format);
+	size = vsnprintf(buf, sizeof(buf), format, ap);
+	va_end(ap);
+
+	if (size < 0) {
+		report(RPT_ERR, "sock_printf: vsnprintf failed");
+		return -1;
+	}
+	if (size > sizeof(buf))
+		report(RPT_WARNING, "sock_printf: vsnprintf truncated message");
+	
+	return sock_send_string(fd, buf);
+}
+
 // Send/receive lines of text
 int
 sock_send_string (int fd, char *string)
 {
-	int len;
-	int offset = 0;
-
-	if (!string)
-		return -1;
-
-	len = strlen (string) ;
-	while (offset != len) {
-		// write isn't guaranteed to send the entire string at once,
-		// so we have to sent it in a loop like this
-		int sent = write (fd, string + offset, len - offset);
-		if (sent == -1) {
-			if (errno != EAGAIN) {
-				perror ("sock_send_string: socket write error");
-				printf ("Message was: %s\n", string);
-				//shutdown(fd, 2);
-				return sent;
-			}
-			continue;
-		} else if (sent == 0) {
-			// when this returns zero, it generally means
-			// we got disconnected
-			return sent + offset;
-		}
-
-		offset += sent;
-	}
-
-	return offset;
+	return sock_send(fd, string, strlen(string));
 }
 
 // Recv gives only one line per call...
@@ -127,7 +146,7 @@ int
 sock_recv_string (int fd, char *dest, size_t maxlen)
 {
 	char *ptr = dest;
-	int recv = 0;
+	int recvBytes = 0;
 
 	if (!dest)
 		return -1;
@@ -135,42 +154,45 @@ sock_recv_string (int fd, char *dest, size_t maxlen)
 		return 0;
 
 	while (1) {
+#ifndef WINSOCK2
 		int err = read (fd, ptr, 1);
+#else
+                int err = recv(fd, ptr, 1, 0);
+#endif
 		if (err == -1) {
 			if (errno == EAGAIN) {
-				if (recv) {
+				if (recvBytes) {
 					// We've begun to read a string, but no bytes are
 					// available.  Loop.
 					continue;
 				}
 				return 0;
 			} else {
-				perror ("sock_recv_string: socket read error");
+				report (RPT_ERR, "sock_recv_string: socket read error");
 				return err;
 			}
 		} else if (err == 0) {
-			return recv;
+			return recvBytes;
 		}
 
-		recv++;
+		recvBytes++;
 
-		if (recv == maxlen || *ptr == 0 || *ptr == 10) {
-			*ptr = 0;
+		// stop at max. bytes allowed, at NUL or at LF
+		if (recvBytes == maxlen || *ptr == '\0' || *ptr == '\n') {
+			*ptr = '\0';
 			break;
 		}
 		ptr++;
 	}
 
-	if (recv == 1 && dest[0] == 0) {
-		// Don't return a null string
+	// Don't return an empty string
+	if (recvBytes == 1 && dest[0] == '\0')
 		return 0;
-	}
 
-	if (recv < maxlen - 1) {
-		dest[recv] = 0;
-	}
+	if (recvBytes < maxlen - 1)
+		dest[recvBytes] = '\0';
 
-	return recv;
+	return recvBytes;
 }
 
 // Send/receive raw data
@@ -185,11 +207,16 @@ sock_send (int fd, void *src, size_t size)
 	while (offset != size) {
 		// write isn't guaranteed to send the entire string at once,
 		// so we have to sent it in a loop like this
-		int sent = write (fd, ((char *) src) + offset, size - offset);
+#ifndef WINSOCK2
+                int sent = write (fd, ((char *) src) + offset, size - offset);
+#else
+                int sent = send(fd, ((char *) src) + offset, size - offset, 0);
+#endif
 		if (sent == -1) {
 			if (errno != EAGAIN) {
-				perror ("sock_send: socket write error");
-				//shutdown(fd, 2);
+				report (RPT_ERR, "sock_send: socket write error");
+				report (RPT_DEBUG, "Message was: '%.*s'", size-offset, (char *) src);
+				//shutdown(fd, SHUT_RDWR);
 				return sent;
 			}
 			continue;
@@ -215,13 +242,93 @@ sock_recv (int fd, void *dest, size_t maxlen)
 	if (maxlen <= 0)
 		return 0;
 
+#ifndef WINSOCK2
 	err = read (fd, dest, maxlen);
+#else
+        err = recv(fd, dest, maxlen, 0);
+#endif
 	if (err < 0) {
-		//fprintf (stderr,"sock_recv: socket read error\n");
-		//shutdown(fd, 2);
+		//report (RPT_DEBUG,"sock_recv: socket read error");
+		//shutdown(fd, SHUT_RDWR);
 		return err;
 	}
-	//debug("sock_recv: Got message \"%s\"\n", (char *)dest);
+	//debug(RPT_DEBUG, "sock_recv: Got message \"%s\"", (char *)dest);
 
 	return err;
+}
+
+/*****************************************************************************/
+
+char*
+sock_geterror(void)
+{
+#ifndef WINSOCK2
+    return strerror(errno);
+#else
+    static char retString[256];
+    long err;
+    char* tmp;
+
+    err = WSAGetLastError();
+
+    sprintf(retString, "Error code %ld: ", err);
+    FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+                  FORMAT_MESSAGE_FROM_SYSTEM | 
+                  FORMAT_MESSAGE_IGNORE_INSERTS,
+                  NULL,
+                  err,
+                  0, /* Default language */
+                  (LPTSTR) &tmp,
+                  0,
+                  NULL);
+
+    /* append the message text after the error code and ensure a terminating
+       character ends the string */
+    strncpy(retString + strlen(retString), tmp, 
+            sizeof(retString) - strlen(retString) - 1);
+    retString[sizeof(retString) - 1] = '\0';
+
+    return retString;
+#endif
+}
+
+/** prints error to logfile and sends it to the client.
+ * @param fd socket
+ * @param message the message to send (without the "huh? ") */
+int sock_send_error(int fd, char* message)
+{
+	// simple: performance penalty isn't worth more work...
+	return sock_printf_error(fd, message);
+}
+
+/** prints printf-like formatted output to logfile and sends it to the
+ * client.
+ * @note don't add a the "huh? " to the message. This is done by this
+ *   method
+ * @param fd socket
+ * @param format a printf format */
+int
+sock_printf_error(int fd, const char *format, .../*args*/ )
+{
+	static const char huh[] = "huh? ";
+	char buf[MAXMSG];
+	va_list ap;
+	int size = 0;
+
+	strcpy(buf, huh);
+	
+	va_start(ap, format);
+	size = vsnprintf(buf + (sizeof(huh)-1), sizeof(buf) - (sizeof(huh)-1), format, ap);
+	buf[sizeof(buf)-1] = '\0';
+	va_end(ap);
+
+	if (size < 0) {
+		report(RPT_ERR, "sock_printf_error: vsnprintf failed");
+		return -1;
+	}
+	if (size >= sizeof(buf) - (sizeof(huh)-1))
+		report(RPT_WARNING, "sock_printf_error: vsnprintf truncated message");
+
+	report(RPT_ERR, "error: %s", buf);
+	return sock_send_string(fd, buf);
 }
