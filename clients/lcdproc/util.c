1d7d69d48969b9201ff82fc68eb11bf5723dab65
marschap <marschap>
2006-05-01 11:42:19 +0000

    sync with HEAD: update clients (complete prev commit)

--- clients/lcdproc/util.c
+++ clients/lcdproc/util.c
@@ -22,7 +22,7 @@
 *  ---
 * 
 *  $Source: /cvsroot/lcdproc/lcdproc/clients/lcdproc/util.c,v $
-*  $Revision: 1.2.2.2 $
+*  $Revision: 1.2.2.3 $
 *  Checked in by: $Author: marschap $
 *
 *******************************************************************************/
