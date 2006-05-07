71385a0e8cbe60c6c0be084f7501ff4ee41f34b4
marschap <marschap>
2006-05-07 17:32:35 +0000

    cleanup iface.c (thanks M. Dolze)

--- clients/lcdproc/util.h
+++ clients/lcdproc/util.h
@@ -22,7 +22,7 @@
 *  ---
 * 
 *  $Source: /cvsroot/lcdproc/lcdproc/clients/lcdproc/util.h,v $
-*  $Revision: 1.1 $
+*  $Revision: 1.2 $
 *  Checked in by: $Author: marschap $
 *
 *******************************************************************************/
@@ -39,6 +39,9 @@ char *sprintf_memory(char *dst, double value, double roundlimit);
 
 /** print a percentage value to a given string */
 char *sprintf_percent(char *dst, double percent);
+
+/** converts value into power-of-x representation */
+char *convert_double(double *value, int base, double roundlimit);
 
 #endif /* LCDPROC_UTIL_H */
 
